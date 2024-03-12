fb772b72576361b2c8cb0200da8f7ca581eb3b30
João <joaogianoni.tds2.senai@gmail.com>
2024-03-12 17:10:31 -0300

    mudei o valor de let

--- question1.js
+++ question1.js
@@ -1,7 +1,7 @@
 //O valor de let x é 0
 let x = 0;
 //O valor de let z é 1000
-let z = 1000;
+let z = 1;
 let resultado = (x - z);
 let mensagem = "O resultado é: ";
 console.log(mensagem + resultado);
